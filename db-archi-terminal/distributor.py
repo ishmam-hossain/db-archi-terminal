cec5c7b5ffb8e9ec57f279fb22d18b1ff18d9f19
ishmam_tonmoy <ishmam@finder-lbs.com>
2019-04-20 15:55:49 +0600

    added mongod client information method

--- db-archi-terminal/distributor.py
+++ db-archi-terminal/distributor.py
@@ -1,7 +1,7 @@
 import pymongo
 import sqlite3
 from typing import NewType, Union
-from mongo_handler import mongo_all_collection_fields, mongo_all_collection_names, mongo_one_collection_fields
+from mongo_handler import mongodb_collection_details, mongodb_collection_names, mongodb_one_collection_fields, mongodb_client_details
 from sqlite3_handler import sqlite3_tables
 
 
@@ -11,8 +11,9 @@ sqlite3_instance = NewType("sqlite3 instance", sqlite3.Cursor)
 
 def get_target_method(_instance):
     cls_to_method = {
-        pymongo.database.Database: mongo_all_collection_names,
-        pymongo.collection.Collection: mongo_one_collection_fields,
+        pymongo.database.Database: mongodb_collection_details,
+        pymongo.collection.Collection: mongodb_one_collection_fields,
+        pymongo.MongoClient: mongodb_client_details,
         sqlite3.Cursor: sqlite3_tables
     }
 
@@ -20,14 +21,15 @@ def get_target_method(_instance):
 
 
 def fetch_data(db_instance: Union[mongodb_instance,
-                                  sqlite3_instance]) -> Union[list, None]:
+                                  sqlite3_instance],
+               _fields=False) -> Union[list, dict, None]:
 
     related_method = get_target_method(db_instance.__class__)
     if related_method:
-        data = related_method(db_instance)
+        data = related_method(db_instance, _fields=_fields)
         return data
 
-    return None
+    return
 
 
 
--- db-archi-terminal/mongo_handler.py
+++ db-archi-terminal/mongo_handler.py
@@ -1,13 +1,13 @@
-def mongo_all_collection_names(db):
-    collections = db.collection_names(include_system_collections=False)
+def mongodb_collection_names(_db, _fields=False):
+    collections = _db.collection_names(include_system_collections=False)
     return collections
 
 
-def mongo_all_collection_fields(db):
-    collections = mongo_all_collection_names(db)
+def mongodb_collection_fields(_db, _fields=False):
+    collections = mongodb_collection_names(_db)
     data = {}
     for collection in collections:
-        cursor = db[collection].find({})
+        cursor = _db[collection].find({})
         for document in cursor:
             data[collection] = [key for key in document.keys()]
             break
@@ -15,20 +15,29 @@ def mongo_all_collection_fields(db):
     return data
 
 
-def mongo_one_collection_fields(collection_instance):
-    cursor = collection_instance.find_one()
+def mongodb_collection_details(_db, _fields=False):
+    if _fields:
+        collections = mongodb_collection_names(_db)
+        data = {}
+        for collection in collections:
+            cursor = _db[collection].find({})
+            for document in cursor:
+                data[collection] = [key for key in document.keys()]
+                break
+
+        return data
+
+    return mongodb_collection_names(_db)
+
+
+def mongodb_one_collection_fields(_collection_instance, _fields=False):
+    cursor = _collection_instance.find_one()
     field_names = [key for key in cursor]
 
     return field_names
 
-# def drop_collection():
-#     collections = get_all_collection_names()
-#
-#     for collection in collections:
-#         try:
-#             db.db[collection].drop()
-#             print(f"Deleted {collection}!")
-#         except:
-#             print(f"Error occurred deleting: {collection}!!!")
 
+def mongodb_client_details(_mongodb_client, _fields=False):
+    print(_mongodb_client)
+    return _mongodb_client
 
