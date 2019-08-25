dedc73184f49ac571a965b5e86916f6d97ddb0cd
ishmam_tonmoy <ishmam@finder-lbs.com>
2019-08-25 13:43:43 +0600

    added type hints

--- db-archi-terminal/distributor.py
+++ db-archi-terminal/distributor.py
@@ -1,6 +1,6 @@
 import pymongo
 import sqlite3
-from typing import NewType, Union
+from typing import NewType, Union, Callable
 from mongo_handler import mongodb_collection_details, mongodb_collection_names,\
     mongodb_one_collection_fields, mongodb_client_details
 from sqlite3_handler import sqlite3_tables
@@ -10,7 +10,7 @@ mongodb_instance = NewType('mongodb instance', pymongo.MongoClient)
 sqlite3_instance = NewType("sqlite3 instance", sqlite3.Cursor)
 
 
-def get_target_method(_instance):
+def get_target_method(_instance: Union[mongodb_instance, sqlite3_instance]) -> Callable:
     cls_to_method = {
         pymongo.database.Database: mongodb_collection_details,
         pymongo.collection.Collection: mongodb_one_collection_fields,
@@ -23,7 +23,7 @@ def get_target_method(_instance):
 
 def fetch_data(db_instance: Union[mongodb_instance,
                                   sqlite3_instance],
-               _fields=False) -> Union[list, dict, None]:
+               _fields: bool = False) -> Union[list, dict, None]:
 
     related_method = get_target_method(db_instance.__class__)
     if related_method:
--- db-archi-terminal/mongo_handler.py
+++ db-archi-terminal/mongo_handler.py
@@ -1,9 +1,12 @@
-def mongodb_collection_names(_db, _fields=False):
+from distributor import mongodb_instance
+
+
+def mongodb_collection_names(_db: mongodb_instance, _fields: bool = False):
     collections = _db.collection_names(include_system_collections=False)
     return collections
 
 
-def mongodb_collection_details(_db, _fields=False):
+def mongodb_collection_details(_db: mongodb_instance, _fields: bool = False):
     if _fields:
         collections = mongodb_collection_names(_db)
         data = {}
@@ -15,7 +18,7 @@ def mongodb_collection_details(_db, _fields=False):
     return mongodb_collection_names(_db)
 
 
-def mongodb_one_collection_fields(_collection_instance, _fields=False):
+def mongodb_one_collection_fields(_collection_instance, _fields: bool = False):
     cursor = _collection_instance.find_one()
     if cursor:
         field_names = [key for key in cursor]
@@ -23,6 +26,6 @@ def mongodb_one_collection_fields(_collection_instance, _fields=False):
     return
 
 
-def mongodb_client_details(_mongodb_client, _fields=False):
+def mongodb_client_details(_mongodb_client, _fields: bool = False):
     return _mongodb_client.server_info()
 
